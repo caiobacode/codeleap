355b38f06f11112f53eac571a0bef8620f0ccc77
caiobacode <caioodsf@gmail.com>
2023-04-19 23:52:26 -0300

    feat: disable btn when empyty fields

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
   ],
   rules: {
     semi: ['error', 'always'],
-    'react/react-in-jsx-scope': 'off'
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off'
   }
 };
--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function SignupForm () {
+export default function SignupForm (history) {
+  const [username, setUsername] = useState('');
+
   return (
     <div className='signup-form'>
       <h1 className='signup-title'>Welcome to CodeLeap network!</h1>
       <div>
-      <span className='signup-text'>Please enter your username</span>
-      <input placeholder='John doe' className='signup-input' type='text'></input>
+        <span className='signup-text'>Please enter your username</span>
+        <input
+          value={username}
+          onChange={({ target: { value } }) => setUsername(value)}
+          placeholder='John doe'
+          className='signup-input'
+          type='text'
+        />
       </div>
       <div className='btn-div'>
-        <button className='signup-btn' type='button'>ENTER</button>
+        <button
+          onClick={() => console.log('btn-enabled')}
+          disabled={username === ''}
+          className='signup-btn'
+          type='button'
+        >
+          ENTER
+        </button>
       </div>
     </div>
   );
