1b1ed41c7d96e68e447dc405acb6e0189ce1b35a
caiobacode <caioodsf@gmail.com>
2023-04-22 20:35:12 -0300

    fix: post date

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -3,12 +3,15 @@ export default function formatDate (date) {
   const actualDate = new Date();
 
   const resultInMinutes = (actualDate - postDateTime) / 1000 / 60;
-  if (resultInMinutes < 2) return '1 minute ago';
-  if (resultInMinutes < 60) return `${parseInt(resultInMinutes)} minutes ago`;
+  if (resultInMinutes < 60) {
+    return `${parseInt(resultInMinutes)} ${resultInMinutes < 2 ? 'minute' : 'minutes'} ago`;
+  }
 
   const resultInHours = resultInMinutes / 60;
-  if (resultInHours < 24) return `${parseInt(resultInHours)} hour$ ago`;
+  if (resultInHours < 24) {
+    return `${parseInt(resultInHours)} ${resultInHours < 2 ? 'hour' : 'hours'} ago`;
+  }
 
   const resultInDays = resultInHours / 24;
-  return `${parseInt(resultInDays)} days ago`;
+  return `${parseInt(resultInDays)} ${resultInDays < 2 ? 'day' : 'days'}  ago`;
 };
